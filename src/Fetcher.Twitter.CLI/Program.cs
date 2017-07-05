56d0efb00c95636f44ce3adb3056c33738857fb5
bardock <fedesanchez89@gmail.com>
2017-07-04 22:49:05 -0300

    initial twitter stream POC

---
+++ src/Fetcher.Twitter.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fetcher.Twitter.CLI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new TwitterFetcher().StartAsync().Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Fetcher.Twitter/TwitterFetcher.cs
@@ -0,0 +1,43 @@
+﻿using LinqToTwitter;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fetcher.Twitter
+{
+    public class TwitterFetcher
+    {
+        public async Task StartAsync()
+        {
+            var auth = new SingleUserAuthorizer
+            {
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    ConsumerKey = "",//ConfigurationManager.AppSettings["twitterConsumerKey"],
+                    ConsumerSecret = "",//ConfigurationManager.AppSettings["twitterConsumerSecret"],
+                    OAuthToken = "",//ConfigurationManager.AppSettings["twitterAccessToken"],
+                    OAuthTokenSecret = ""//ConfigurationManager.AppSettings["twitterAccessTokenSecret"]
+                }
+            };
+
+            Console.WriteLine("\nStreamed Content: \n");
+            int count = 0;
+
+            var twitterCtx = new TwitterContext(auth);
+            await
+                (from strm in twitterCtx.Streaming
+                 where strm.Type == StreamingType.Filter &&
+                 strm.Track == "twitter"//"#ligamundial"
+                 select strm)
+                .StartAsync(strm =>
+                {
+                    Console.WriteLine(strm.Content + "\n\n");
+
+                    if (count++ >= 5)
+                        strm.CloseStream();
+
+                    return Task.CompletedTask;
+                });
+        }
+    }
+}
